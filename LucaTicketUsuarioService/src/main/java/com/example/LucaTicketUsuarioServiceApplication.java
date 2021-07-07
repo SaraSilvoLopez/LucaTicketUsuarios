f735eaa94b7cb95367945245846273c08fb6bb0c
Jennifer <perezmartinezjennifer05@gmail.com>
2021-07-07 13:32:59 +0200

    Usuarios Básico

--- LucaTicketUsuarioService/src/main/java/com/example/LucaTicketUsuarioServiceApplication.java
+++ LucaTicketUsuarioService/src/main/java/com/example/LucaTicketUsuarioServiceApplication.java
@@ -1,9 +1,12 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LucaTicketUsuarioServiceApplication {
 
 	public static void main(String[] args) {
--- LucaTicketUsuarioService/src/main/java/com/example/repository/UsuarioRepository.java
+++ LucaTicketUsuarioService/src/main/java/com/example/repository/UsuarioRepository.java
@@ -1,5 +1,22 @@
 package com.example.repository;
+import com.example.model.Usuario;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * @ClassName UsuarioRepository
+ *
+ * @author Jennifer Pérez
+ * @date 06 jul. 2021
+ * 
+ * @version 2.0
+ */
+
+
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+	//Usuario addUsuario(String nombre);
 }
