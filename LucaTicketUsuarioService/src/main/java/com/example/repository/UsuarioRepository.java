08ef17804d75a7e3b1bd8fa6f0976927fedfac2e
Sara Silvo López <76155274+SaraSilvoLopez@users.noreply.github.com>
2021-07-07 13:47:32 +0200

    Revert "Usuarios Básico"

--- LucaTicketUsuarioService/src/main/java/com/example/repository/UsuarioRepository.java
+++ LucaTicketUsuarioService/src/main/java/com/example/repository/UsuarioRepository.java
@@ -1,22 +1,5 @@
 package com.example.repository;
-import com.example.model.Usuario;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public interface UsuarioRepository {
 
-/**
- * @ClassName UsuarioRepository
- *
- * @author Jennifer Pérez
- * @date 06 jul. 2021
- * 
- * @version 2.0
- */
-
-
-@Repository
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-	//Usuario addUsuario(String nombre);
 }
--- LucaTicketUsuarioService/src/main/java/com/example/service/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.model.Usuario;
-
-public interface UsuarioService {
-	public Usuario save(Usuario usuario);
-	//public Usuario addUsuario();
-
-}
