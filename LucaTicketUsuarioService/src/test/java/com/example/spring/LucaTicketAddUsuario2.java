f4890a532d93def77b2d880d8d964a93588c81b3
SaraSilvoLopez <you@example.com>
2021-07-08 12:21:56 +0200

    cambio nombre paquete

--- LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicketAddUsuario2.java
+++ LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicketAddUsuario2.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.spring;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
--- LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicket_addUsuarioTest.java
+++ LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicket_addUsuarioTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.spring;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
