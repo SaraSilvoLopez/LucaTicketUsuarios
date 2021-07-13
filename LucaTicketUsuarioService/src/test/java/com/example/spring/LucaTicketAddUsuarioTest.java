65caf6673d456ab0f04549c94cd55bc76e52914e
SaraSilvoLopez <you@example.com>
2021-07-13 15:22:19 +0200

    test jennifer

--- LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicketAddUsuarioTest.java
+++ LucaTicketUsuarioService/src/test/java/com/example/spring/LucaTicketAddUsuarioTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LucaTicketUsuarioServiceApplicationTests {
+class LucaTicketAddUsuarioTest {
 
 	@Test
 	void contextLoads() {
---
+++ LucaTicketUsuarioService/src/test/java/com/example/spring/LucaticketGetUsuarioTest.java
@@ -0,0 +1,30 @@
+package com.example.spring;
+
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+
+@SpringBootConfiguration
+public class LucaticketGetUsuarioTest {
+
+
+
+@Test
+public void getUsuarioTest() {
+given().log().all()
+.port(2222).get("/1")
+.then().log().all()
+.statusCode(200).body("id", is(1));
+}
+
+
+
+}
